0f5d298e73df63bb48f1feeb6c7cad71b83bc98e
Sebastiaan Visser <code@fvisser.nl>
2012-11-24 01:57:09 +0100

    Added list of HTML5 elements.

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@ module Main where
 import Data.Monoid
 import Prelude (($), IO)
 
+import Element
 import Selector
 import Rules
 import Render
@@ -11,14 +12,14 @@ import Render
 main :: IO ()
 main = renderAndPrint $
 
-  "body" > ".section" + "a" `rules`
+  body > div + a `rules`
     do "font-size" -: "12px"
        "color"     -: "12px"
        "color"     -: "12px"
 
     <>
 
-  "body" > ".section" + "a" `rules`
+  body > ".section" + a `rules`
     do "font-size" -: "12px"
        "color"     -: "12px"
 
