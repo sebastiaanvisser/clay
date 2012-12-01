0e66bb25fb5786175f31e47cfc8152ebdd57a640
Sebastiaan Visser <code@fvisser.nl>
2012-12-01 14:34:24 +0100

    Added Val instances for Double, Value and Mabye.
    And added monoid instance for values.

--- src/Property.hs
+++ src/Property.hs
@@ -1,29 +1,33 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, GeneralizedNewtypeDeriving #-}
 module Property where
 
 import Data.String
 import Data.Text (Text, intercalate)
 import Control.Monad.Writer
 
--------------------------------------------------------------------------------
-
 newtype Key a = Key { unKey :: Text }
 
 instance IsString (Key a) where
   fromString = Key . fromString
 
--------------------------------------------------------------------------------
-
 newtype Value = Value { unValue :: Text }
+  deriving (Monoid, IsString)
 
 class Val a where
   value :: a -> Value
 
-instance IsString Value where
-  fromString = Value . fromString
-
 instance Val Text where
   value = Value
+
+instance Val Double where
+  value = fromString . show
+
+instance Val Value where
+  value = id
+
+instance Val a => Val (Maybe a) where
+  value Nothing  = "none"
+  value (Just a) = value a
 
 instance (Val a, Val b) => Val (a, b) where
   value (a, b) =
