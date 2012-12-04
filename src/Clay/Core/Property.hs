7fe785de4ef84fbebc58a573fe23f1a468dc728e
Sebastiaan Visser <code@fvisser.nl>
2012-12-04 17:41:54 +0100

    Added some more combined value instances.

--- src/Clay/Core/Property.hs
+++ src/Clay/Core/Property.hs
@@ -40,6 +40,18 @@ instance (Val a, Val b, Val c) => Val (a, b, c) where
 instance (Val a, Val b, Val c, Val d) => Val (a, b, c, d) where
   value (a, b, c, d) = value (a, (b, (c, d)))
 
+instance (Val a, Val b, Val c, Val d, Val e) => Val (a, b, c, d, e) where
+  value (a, b, c, d, e) = value (a, (b, (c, (d, e))))
+
+instance (Val a, Val b, Val c, Val d, Val e, Val f) => Val (a, b, c, d, e, f) where
+  value (a, b, c, d, e, f) = value (a, (b, (c, (d, (e, f)))))
+
+instance (Val a, Val b, Val c, Val d, Val e, Val f, Val g) => Val (a, b, c, d, e, f, g) where
+  value (a, b, c, d, e, f, g) = value (a, (b, (c, (d, (e, (f, g))))))
+
+instance (Val a, Val b, Val c, Val d, Val e, Val f, Val g, Val h) => Val (a, b, c, d, e, f, g, h) where
+  value (a, b, c, d, e, f, g, h) = value (a, (b, (c, (d, (e, (f, (g, h)))))))
+
 instance Val a => Val [a] where
   value xs = Value (intercalate "," (map (unValue . value) xs))
 
