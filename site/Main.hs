b3437d095f3e4a7b523a9b20e7163ba18ceb4e94
Sebastiaan Visser <code@fvisser.nl>
2012-12-06 10:29:07 +0100

    Updated a now colorful demo.

--- site/Main.hs
+++ site/Main.hs
@@ -1,54 +1,99 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Data.Monoid
+import Control.Monad
+import Data.Text (pack)
 import Prelude hiding (div)
-import Clay
+import Clay hiding (i, s)
 
 main :: IO ()
 main = css $
-  do overal
-     theFooter
+  do logo
 
   where
+    s = 80
+    m = 20
 
-    overal =
-      do body <> html ?
-           do height (pct 100)
-              resetSpacing
+    logo = "#logo" ?
+      do blocked 400 100 (m * 4 + (m + s) * 3) (m * 4 + (m + s) * 3)
+         background (grayish 240)
 
-         body <> section ?
-           do backgroundColor (grayish 220)
-              fonts
+         forM_ [0..3] $ \x ->
+           forM_ [0..3] $ \y ->
+             do let idx = (pack . show) (1 + y * 4 + x)
+                    clr = cycle [ "#78e700"
+                                , "#00b454"
+                                , "#ff3900"
+                                , "#de0052"
+                                ] !! fromInteger y
+                square idx (m * y + x * (s + m))
+                           (m * x + y * (s + m))
+                           ( clr -. 40 +. (x * 40) )
 
-              ("href" $= "/page") &
-                color (rgba 255 128 0 80)
+         "-webkit-transform" -: "scaleY(0.5)"
 
-              hover &
-                  backgroundColor green
+         div <?
+           do position absolute
+              background red
+              "-webkit-transform" -: "rotate(45deg)"
 
-              "#container" ?
-                do minHeight (pct 100)
-                   minWidth  (pct 100)
 
-    fonts =
-      do fontFamily "Helvetica, sans-serif"
-         fontColor  (rgb 40 30 0)
+    square i x y c = ".square" `with` nthChild i ?
+      do blocked x y s s
+         background (hGradient (c -. 100) c)
+         "-webkit-box-shadow" -: "0 0 40px rgba(80,40,0, 0.8)"
+         font ( FontOptional (Just bold) Nothing (Just italic)
+              , FontMandatory (px 50) (Just (px 60)) ["Helvetica", sansSerif]
+              )
+         color (setA 80 white)
+         "-webkit-box-sizing" -: "border-box"
+         sym2 padding (px 2) (px 22)
 
-    theFooter =
-      footer ?
-        do backgroundColor "#ff880020"
-           color           white
-           position        absolute
-           bottom          (px 0)
-           width           (pct 100)
-           height          (px 200)
-           abbr <? p ? color red
+         hover &
+           do color white
+              div <?
+                do background (vGradient (setA 0 white) (setA 100 white))
 
-           a ?
-             display inline
+         div <?
+           do position absolute
+              background (vGradient (setA 0 white) (setA 40 white))
+              borderBottomLeftRadius  (pct 100) (pct 100)
+              borderBottomRightRadius (pct  40) (pct  15)
+              [left, top, right, bottom] `forM_` ($ 4)
 
-    resetSpacing =
-      do sym4 margin  (px 0)
-         sym4 padding (px 0)
+         before &
+           do toTheRight m
+              background (vGradient (c -. 10) (c -. 60))
+              "-webkit-transform" -: "translate(0,10px) skew(0deg,45deg)"
+              "-webkit-box-shadow" -: "0 0 40px rgba(80,40,0, 1)"
+         after &
+           do toTheBottom m
+              background (hGradient (c +. 10) (c +. 80))
+              "-webkit-transform" -: "translate(10px,0) skew(45deg,0deg)"
+              "-webkit-box-shadow" -: "0 0 40px rgba(80,40,0, 1)"
+
+    blocked x y w h =
+      do position absolute
+         left   (px x)
+         top    (px y)
+         width  (px w)
+         height (px h)
+
+toTheRight :: Integer -> Css
+toTheRight h =
+  do position absolute
+     "content" -: "''"
+     top    (px 0)
+     bottom (px 0)
+     right  (px (-h))
+     width  (px h)
+
+toTheBottom :: Integer -> Css
+toTheBottom h =
+  do position absolute
+     "content" -: "''"
+     left   (px 0)
+     right  (px 0)
+     bottom (px (-h))
+     height (px h)
 
