f7cc19ef7b49e582b6300f0ac113ccee3c7c79bc
Sebastiaan Visser <code@fvisser.nl>
2012-12-08 11:40:10 +0100

    Updated example with vendor prefixed versions.

--- site/example/Main.hs
+++ site/example/Main.hs
@@ -20,20 +20,15 @@ main = css $
          forM_ [0..3] $ \x ->
            forM_ [0..3] $ \y ->
              do let idx = (pack . show) (1 + y * 4 + x)
-                    clr = cycle [ "#78e700"
-                                , "#00b454"
-                                , "#ff3900"
-                                , "#de0052"
-                                ] !! fromInteger y
+                    clr = cycle [ "#78e700", "#00b454", "#ff3900", "#de0052" ] !! fromInteger y
                 square idx (m * y + x * (s + m))
                            (m * x + y * (s + m))
                            (clr -. 50 +. (x * 50))
 
-         transform (scaleY 0.5)
+         transform (scaleY 0.6)
 
          div <?
            do position absolute
-              background red
               transform (rotateZ (deg 45))
 
 
@@ -41,11 +36,13 @@ main = css $
       do blocked x y s s
          background (hGradient (c -. 100) c)
          "-webkit-box-shadow" -: "0 0 40px rgba(80,40,0, 0.8)"
+         "box-shadow" -: "0 0 40px rgba(80,40,0, 0.8)"
          font ( FontOptional (Just bold) Nothing (Just italic)
               , FontMandatory (px 50) (Just (px 60)) ["Helvetica", sansSerif]
               )
          color (setA 80 white)
          "-webkit-box-sizing" -: "border-box"
+         "-moz-box-sizing" -: "border-box"
          sym2 padding (px 2) (px 22)
 
          hover &
@@ -65,11 +62,13 @@ main = css $
               background (vGradient (c -. 10) (c -. 60))
               transforms [translate (px 0) (px 10), skew (deg 0) (deg 45)]
               "-webkit-box-shadow" -: "0 0 40px rgba(80,40,0, 1)"
+              "box-shadow" -: "0 0 40px rgba(80,40,0, 1)"
          after &
            do toTheBottom m
               background (hGradient (c +. 10) (c +. 80))
               transforms [translate (px 10) (px 0), skew (deg 45) (deg 0)]
               "-webkit-box-shadow" -: "0 0 40px rgba(80,40,0, 1)"
+              "box-shadow" -: "0 0 40px rgba(80,40,0, 1)"
 
     blocked x y w h =
       do position absolute
