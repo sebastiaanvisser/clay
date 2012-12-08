30a038ea765cb0cb142f5383e683640538971167
Sebastiaan Visser <code@fvisser.nl>
2012-12-08 14:10:28 +0100

    Changed demo to produce smaller code.

--- site/example/Main.hs
+++ site/example/Main.hs
@@ -15,37 +15,46 @@ main = css $
     m = 60
 
     logo = "#logo" ?
-      do blocked 400 100 (m * 4 + (m + s) * 3) (m * 4 + (m + s) * 3)
-
-         forM_ [0..3] $ \x ->
-           forM_ [0..3] $ \y ->
-             do let idx = (pack . show) (1 + y * 4 + x)
-                    clr = cycle [ "#78e700", "#00b454", "#ff3900", "#de0052" ] !! fromInteger y
-                square idx (m * y + x * (s + m))
-                           (m * x + y * (s + m))
-                           (clr -. 50 +. (x * 50))
-
+      do rectangular 400 100 (m * 4 + (m + s) * 3) (m * 4 + (m + s) * 3)
+         position absolute
+         boxSizing borderBox
          transform (scaleY 0.5)
 
          "div" <?
            do position absolute
               transform (rotateZ (deg 45))
 
+         square
+         forM_ [0..3] $ \x ->
+           forM_ [0..3] $ \y ->
+             do let idx = (pack . show) (1 + y * 4 + x)
+                    clr = cycle [ "#78e700", "#00b454", "#ff3900", "#de0052" ] !! fromInteger y
+                squareI idx (m * y + x * (s + m))
+                            (m * x + y * (s + m))
+                            (clr -. 50 +. (x * 50))
 
-    square i x y c = ".square" `with` nthChild i ?
-      do blocked x y s s
-         background (hGradient (c -. 100) c)
-         boxShadow 0 0 (px 50) c
-         font ( FontOptional (Just bold) Nothing (Just italic)
+    square = ".square" ?
+      do font ( FontOptional (Just bold) Nothing (Just italic)
               , FontMandatory (px 50) (Just (px 60)) ["Helvetica", sansSerif]
               )
          color (setA 80 white)
          sym2 padding (px 2) (px 22)
+         position absolute
+         boxSizing borderBox
 
          hover &
            do color white
-              "div" <?
-                do background (vGradient (setA 0 white) (setA 100 white))
+              "div" <? background (vGradient (setA 0 white) (setA 100 white))
+
+         before &
+           do toTheRight m
+              transforms [translate (px 0) (px (div m 2)), skew (deg 0) (deg 45)]
+              boxShadow 0 0 (px 40) black
+
+         after &
+           do toTheBottom m
+              transforms [translate (px (div m 2)) (px 0), skew (deg 45) (deg 0)]
+              boxShadow 0 0 (px 40) black
 
          "div" <?
            do position absolute
@@ -54,42 +63,34 @@ main = css $
               borderBottomRightRadius (pct  40) (pct  15)
               [left, top, right, bottom] `forM_` ($ 4)
 
-         before &
-           do toTheRight m
-              background (vGradient (c -. 10) (c -. 60))
-              transforms [translate (px 0) (px (div m 2)), skew (deg 0) (deg 45)]
-              boxShadow 0 0 (px 40) black
-         after &
-           do toTheBottom m
-              background (hGradient (c +. 10) (c +. 80))
-              transforms [translate (px (div m 2)) (px 0), skew (deg 45) (deg 0)]
-              boxShadow 0 0 (px 40) black
+    squareI i x y c = ".square" `with` nthChild i ?
+      do rectangular x y s s
+         background (hGradient (c -. 100) c)
+         boxShadow 0 0 (px 50) c
+         before & background (vGradient (c -. 10) (c -. 60))
+         after  & background (hGradient (c +. 10) (c +. 80))
 
-    blocked x y w h =
-      do position absolute
-         boxSizing borderBox
-         left   (px x)
+    rectangular x y w h =
+      do left   (px x)
          top    (px y)
          width  (px w)
          height (px h)
 
-toTheRight :: Integer -> Css
-toTheRight h =
-  do position absolute
-     content (stringContent "@")
-     overflow hidden
-     top    (px 0)
-     bottom (px 0)
-     right  (px (-h))
-     width  (px h)
+    toTheRight h =
+      do position absolute
+         content (stringContent "@")
+         overflow hidden
+         top    (px 0)
+         bottom (px 0)
+         right  (px (-h))
+         width  (px h)
 
-toTheBottom :: Integer -> Css
-toTheBottom h =
-  do position absolute
-     content (stringContent "!#!")
-     overflow hidden
-     left   (px 0)
-     right  (px 0)
-     bottom (px (-h))
-     height (px h)
+    toTheBottom h =
+      do position absolute
+         content (stringContent "!#!")
+         overflow hidden
+         left   (px 0)
+         right  (px 0)
+         bottom (px (-h))
+         height (px h)
 
