7be7333a1d178db1ce1abfa03a52f18f19f21929
Sebastiaan Visser <code@fvisser.nl>
2012-12-09 13:29:15 +0100

    Put selection rendering into the render module.

--- src/Clay/Render.hs
+++ src/Clay/Render.hs
@@ -12,17 +12,19 @@ import Control.Applicative
 import Control.Monad.Writer
 import Data.Either
 import Data.Foldable (foldMap)
+import Data.List (sort)
 import Data.Text (Text)
 import Data.Text.Lazy.Builder
+import Prelude hiding (filter)
 
 import qualified Data.Text         as Text
 import qualified Data.Text.Lazy.IO as Text
 
-import Clay.Rule
+import Clay.Rule hiding (Child)
 import Clay.Property
-import Clay.Selector hiding (Child)
+import Clay.Selector
 
-import qualified Clay.Selector as Selector
+import qualified Clay.Rule as Rule
 
 data Config = Config
   { indent  :: Builder
@@ -70,17 +72,17 @@ rule cfg sel props =
       ]
 
 renderRule :: [Rule] -> Builder
-renderRule = Selector.render . merger
+renderRule = selector . merger
 
 merger :: [Rule] -> Selector
 merger []     = error "this should be fixed!"
 merger (x:xs) =
   case x of
-    Child s -> case xs of [] -> s; _  -> merger xs |> s
-    Sub  s  -> case xs of [] -> s; _  -> merger xs `deep` s
-    Root s  -> s `deep` merger xs
-    Pop  i  -> merger (drop i (x:xs))
-    Self f  -> merger xs `with` f
+    Rule.Child s -> case xs of [] -> s; _  -> merger xs |> s
+    Sub        s  -> case xs of [] -> s; _  -> merger xs `deep` s
+    Root       s  -> s `deep` merger xs
+    Pop        i  -> merger (drop i (x:xs))
+    Self       f  -> merger xs `with` f
 
 collect :: (Key (), Value) -> [Either Text (Text, Text)]
 collect (Key ky, Value vl) =
@@ -103,4 +105,31 @@ properties cfg xs =
             let pad = if align cfg then fromText (Text.replicate (width - Text.length k) " ") else ""
              in mconcat [ind, fromText k, pad, ":", sep cfg, fromText v, ";", new]
 
+selector :: Selector -> Builder
+selector = intersperse ",\n" . rec
+  where rec (In (Filtered ft p)) = (<> filter ft) <$>
+          case p of
+            Star           -> ["*"]
+            Elem t         -> [fromText t]
+            Child      a b -> ins " > " <$> rec a <*> rec b
+            Deep       a b -> ins " "   <$> rec a <*> rec b
+            Adjacent   a b -> ins " + " <$> rec a <*> rec b
+            Combined   a b -> join ((:) <$> rec a <*> (pure <$> rec b))
+          where ins s a b = (a <> s <> b)
+
+predicate :: Predicate -> Builder
+predicate ft = mconcat $
+  case ft of
+    Id         a   -> [ "#", fromText a                                             ]
+    Class      a   -> [ ".", fromText a                                             ]
+    Attr       a   -> [ "[", fromText a,                     "]"                    ]
+    AttrVal    a v -> [ "[", fromText a,  "='", fromText v, "']"                    ]
+    AttrEnds   a v -> [ "[", fromText a, "$='", fromText v, "']"                    ]
+    AttrSpace  a v -> [ "[", fromText a, "~='", fromText v, "']"                    ]
+    AttrHyph   a v -> [ "[", fromText a, "|='", fromText v, "']"                    ]
+    Pseudo     a   -> [ ":", fromText a                                             ]
+    PseudoFunc a p -> [ ":", fromText a, "(", intersperse "," (map fromText p), ")" ]
+
+filter :: Filter -> Builder
+filter = foldMap predicate . sort . unFilter
 
--- src/Clay/Selector.hs
+++ src/Clay/Selector.hs
@@ -5,19 +5,13 @@
   #-}
 module Clay.Selector where
 
-import Data.String
-import Prelude hiding (foldl)
 import Control.Applicative
-import Control.Monad
-import Data.Foldable hiding (elem)
-import Data.List (sort)
 import Data.Monoid
+import Data.String
 import Data.Text (Text)
-import Data.Text.Lazy.Builder hiding (fromString)
+import Prelude hiding (foldl)
 
 import qualified Data.Text as Text
-
-import Clay.Property (intersperse)
 
 -- The generic selector DSL.
 
@@ -77,24 +71,8 @@ data Predicate
   | PseudoFunc Text [Text]
   deriving (Eq, Ord, Show)
 
-renderPredicate :: Predicate -> Builder
-renderPredicate ft = mconcat $
-  case ft of
-    Id         a   -> [ "#", fromText a                                              ]
-    Class      a   -> [ ".", fromText a                                              ]
-    Attr       a   -> [ "[", fromText a,                     "]"                     ]
-    AttrVal    a v -> [ "[", fromText a,  "='", fromText v, "']"                     ]
-    AttrEnds   a v -> [ "[", fromText a, "$='", fromText v, "']"                     ]
-    AttrSpace  a v -> [ "[", fromText a, "~='", fromText v, "']"                     ]
-    AttrHyph   a v -> [ "[", fromText a, "|='", fromText v, "']"                     ]
-    Pseudo     a   -> [ ":", fromText a                                              ]
-    PseudoFunc a p -> [ ":", fromText a, "(", intersperse "," (map fromText p) , ")" ]
-
 newtype Filter = Filter { unFilter :: [Predicate] }
   deriving (Monoid, Eq, Ord, Show)
-
-renderFilter :: Filter -> Builder
-renderFilter = foldMap renderPredicate . sort . unFilter
 
 instance IsString Filter where
   fromString = predicateFromText . fromString
@@ -112,31 +90,17 @@ predicateFromText t = Filter $
 
 data Path f
   = Star
-  | Elem       Text
-  | Child      f f
-  | Deep       f f
-  | Adjacent   f f
-  | Combined   f f
+  | Elem      Text
+  | Child     f f
+  | Deep      f f
+  | Adjacent  f f
+  | Combined  f f
 
 newtype Fix f = In { out :: f (Fix f) }
 
 data Filtered a = Filtered Filter (Path a)
 
 type Selector = Fix Filtered
-
-render :: Selector -> Builder
-render = intersperse ",\n" . renderSelector
-
-renderSelector :: Selector -> [Builder]
-renderSelector (In (Filtered ft p)) = (<> renderFilter ft) <$>
-  case p of
-    Star           -> ["*"]
-    Elem t         -> [fromText t]
-    Child      a b -> ins " > " <$> renderSelector a <*> renderSelector b
-    Deep       a b -> ins " "   <$> renderSelector a <*> renderSelector b
-    Adjacent   a b -> ins " + " <$> renderSelector a <*> renderSelector b
-    Combined   a b -> join ((:) <$> renderSelector a <*> (pure <$> renderSelector b))
-  where ins s a b = (a <> s <> b)
 
 instance IsString (Fix Filtered) where
   fromString = text . fromString
