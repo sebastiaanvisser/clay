d3508f35d6c13145b6b280673b5feeb876bd9f00
Sebastiaan Visser <code@fvisser.nl>
2012-12-22 22:20:33 +0100

    Generated new site.

--- code.js
+++ code.js
@@ -3,7 +3,7 @@ $(window).scroll
     {
       $( "header" ).css
         ( "top"
-        , Math.max(-280, Math.min(0, -$($.browser.mozilla ? window : "body").scrollTop()))
+        , Math.max(-230, Math.min(0, -$($.browser.mozilla ? window : "body").scrollTop()))
         );
     }
   );
@@ -15,7 +15,7 @@ $("a").click
       var target = $("*[name=" + $(ev.target).attr("href").substr(1) + "]");
 
       $("html, body").animate
-        ( { scrollTop : $(target).offset().top - 80 }
+        ( { scrollTop : $(target).offset().top - 68 }
         , ev.shiftKey ? 2500 : 500
         );
       ev.preventDefault();
