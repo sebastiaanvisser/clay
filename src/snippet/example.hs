f303cba3141bac8c15957b39bdd0af4d229c2f6e
Sebastiaan Visser <code@fvisser.nl>
2013-01-17 19:12:43 +0100

    Whitespace.

--- src/snippet/example.hs
+++ src/snippet/example.hs
@@ -5,12 +5,12 @@ import Clay
 
 menu :: Css
 menu = header |> nav ?
- do background    white
-    color         "#04a"
-    fontSize      (px 24)
-    padding       20 0 20 0
-    textTransform uppercase
-    position      absolute
-    left          0
-    right         0
-    bottom        (px (-72))
+  do background    white
+     color         "#04a"
+     fontSize      (px 24)
+     padding       20 0 20 0
+     textTransform uppercase
+     position      absolute
+     left          0
+     right         0
+     bottom        (px (-72))
