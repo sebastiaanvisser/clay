4d31827a1d58f12ed01fc65f4a5044a30a813f92
James Fisher <jameshfisher@gmail.com>
2013-05-29 23:29:44 +0100

    Add test framework with one unit test
    Fixes GH-63

---
+++ src/Test.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Test.Framework (defaultMain)
+import Test.Framework.Providers.HUnit (testCase)
+import Test.HUnit.Base ((@?=))
+
+import Clay.Render (renderWith, compact)
+
+main :: IO ()
+main = defaultMain
+  [ testCase "empty Clay produces empty compact CSS" $ renderWith compact (return ()) @?= ""
+  ]
