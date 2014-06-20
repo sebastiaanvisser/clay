dbc21b3fc782135fedb35e84364c89d8d42ab473
Heather <heather@live.ru>
2014-06-20 11:28:14 +0400

    ListStyle abstraction (not all values added)

--- src/Clay/List.hs
+++ src/Clay/List.hs
@@ -5,6 +5,15 @@ module Clay.List
 , disc
 , decimal
 , hiragana
+
+, ListStylePosition
+, listStylePosition
+, outside
+
+, ListStyleImage
+, listStyleImage
+
+, listStyle
 )
 where
 
@@ -23,3 +32,22 @@ hiragana    = ListStyleType "hiragana"
 
 listStyleType :: ListStyleType -> Css
 listStyleType = key "list-style-type"
+
+newtype ListStylePosition = ListStylePosition Value
+  deriving (Val, Initial, Inherit, None, Other)
+
+outside :: ListStylePosition
+
+outside = ListStylePosition "outside"
+
+listStylePosition :: ListStylePosition -> Css
+listStylePosition = key "list-style-position"
+
+newtype ListStyleImage = ListStyleImage Value
+  deriving (Val, Initial, Inherit, None, Other)
+
+listStyleImage :: ListStyleImage -> Css
+listStyleImage = key "list-style-image"
+
+listStyle :: ListStyleType -> ListStylePosition -> ListStyleImage -> Css
+listStyle a b c = key "list-style" (a ! b ! c)
