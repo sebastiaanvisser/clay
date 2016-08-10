c84c9784227394deef9823493624f7f2fda6c910
Martin Kolinek <martin.kolinek@erni.sk>
2016-08-10 13:30:21 +0200

    Introduced sum for sizes as an experiment for calc

---
+++ spec/Clay/SizeSpec.hs
@@ -0,0 +1,34 @@
+{-#LANGUAGE OverloadedStrings#-}
+
+module Clay.SizeSpec where
+
+import Clay.Size
+import Clay.Property
+import Clay.Common
+
+import Test.Hspec
+import Data.Text
+import Data.List
+
+sizeRepr :: Size a -> Text
+sizeRepr = plain . unValue . value
+
+hasAllPrefixes :: Val a => a -> Bool
+hasAllPrefixes a = checkPrefixed ((unValue . value) a) browsers
+  where checkPrefixed (Prefixed pa) (Prefixed pb) = sort (fmap fst pa) == sort (fmap fst pb)
+        checkPrefixed _ _ = False
+
+spec :: Spec
+spec = do
+  describe "simple sizes" $ do
+    it "returns 1px for (px 1)" $
+      sizeRepr (px 1) `shouldBe` "1px"
+    it "return 50% for (pct 50)" $
+      sizeRepr (pct 50) `shouldBe` "50%"
+  describe "calc addition" $ do
+    it "returns proper calc for simple sum" $
+      sizeRepr (em 2 @+@ px 1) `shouldBe` "calc(2em + 1px)"
+    it "returns calc for nested sum" $
+      sizeRepr (em 2 @+@ pt 1 @+@ px 3) `shouldBe` "calc((2em + 1pt) + 3px)"
+    it "returns prefixed calc for simple sum" $
+      (em 2 @+@ pt 2) `shouldSatisfy` hasAllPrefixes
--- src/Clay/Size.hs
+++ src/Clay/Size.hs
@@ -3,6 +3,8 @@
   , OverloadedStrings
   , GeneralizedNewtypeDeriving
   , FlexibleInstances
+  , ExistentialQuantification
+  , StandaloneDeriving
   #-}
 module Clay.Size
 (
@@ -31,6 +33,10 @@ module Clay.Size
 , vmin
 , vmax
 
+-- * Calculation operators for calc
+
+, (@+@)
+
 -- * Shorthands for properties that can be applied separately to each box side.
 
 , sym
@@ -57,6 +63,7 @@ where
 
 import Data.Monoid
 import Prelude hiding (rem)
+import Data.Text (Text)
 
 import Clay.Common
 import Clay.Property
@@ -70,63 +77,77 @@ data LengthUnit
 -- | Sizes can be given in percentages.
 data Percentage
 
-newtype Size a = Size Value
-  deriving (Val, Auto, Normal, Inherit, None, Other)
+data Size a = SimpleSize Text | forall b c. SumSize (Size b) (Size c)
+
+deriving instance Show (Size a)
+
+sizeToText :: Size a -> Text
+sizeToText (SimpleSize txt) = txt
+sizeToText (SumSize a b) = "(" <> sizeToText a <> " + " <> sizeToText b <> ")"
+
+instance Val (Size a) where
+  value (SimpleSize a) = value a
+  value s@(SumSize _ _) = Value $ browsers <> Plain ("calc" <> sizeToText s)
+
+instance Auto (Size a) where auto = Clay.Common.auto
+instance Normal (Size a) where normal = Clay.Common.normal
+instance Inherit (Size a) where inherit = Clay.Common.inherit
+instance None (Size a) where none = Clay.Common.none
 
 -- | Zero size.
 nil :: Size a
-nil = Size "0"
+nil = SimpleSize "0"
 
 -- | Unitless size (as recommended for line-height).
 unitless :: Double -> Size a
-unitless i = Size (value i)
+unitless i = SimpleSize ((plain . unValue . value) i)
 
 cm, mm, inches, px, pt, pc :: Double -> Size LengthUnit
 
 -- | Size in centimeters.
-cm i = Size (value i <> "cm")
+cm i = SimpleSize (cssDoubleText i <> "cm")
 
 -- | Size in millimeters.
-mm i = Size (value i <> "mm")
+mm i = SimpleSize (cssDoubleText i <> "mm")
 
 -- | Size in inches (1in = 2.54 cm).
-inches i = Size (value i <> "in")
+inches i = SimpleSize (cssDoubleText i <> "in")
 
 -- | Size in pixels.
-px i = Size (value i <> "px")
+px i = SimpleSize (cssDoubleText i <> "px")
 
 -- | Size in points (1pt = 1/72 of 1in).
-pt i = Size (value i <> "pt")
+pt i = SimpleSize (cssDoubleText i <> "pt")
 
 -- | Size in picas (1pc = 12pt).
-pc i = Size (value i <> "pc")
+pc i = SimpleSize (cssDoubleText i <> "pc")
 
 em, ex, rem, vw, vh, vmin, vmax :: Double -> Size LengthUnit
 
--- | Size in em's (computed value of the font-size).
-em i = Size (value i <> "em")
+-- | Size in em's (computed cssDoubleText of the font-size).
+em i = SimpleSize (cssDoubleText i <> "em")
 
--- | Size in ex'es (x-height of the first avaliable font).
-ex i = Size (value i <> "ex")
+-- | SimpleSize in ex'es (x-height of the first avaliable font).
+ex i = SimpleSize (cssDoubleText i <> "ex")
 
--- | Size in rem's (em's, but always relative to the root element).
-rem i = Size (value i <> "rem")
+-- | SimpleSize in rem's (em's, but always relative to the root element).
+rem i = SimpleSize (cssDoubleText i <> "rem")
 
--- | Size in vw's (1vw = 1% of viewport width).
-vw i = Size (value i <> "vw")
+-- | SimpleSize in vw's (1vw = 1% of viewport width).
+vw i = SimpleSize (cssDoubleText i <> "vw")
 
--- | Size in vh's (1vh = 1% of viewport height).
-vh i = Size (value i <> "vh")
+-- | SimpleSize in vh's (1vh = 1% of viewport height).
+vh i = SimpleSize (cssDoubleText i <> "vh")
 
--- | Size in vmin's (the smaller of vw or vh).
-vmin i = Size (value i <> "vmin")
+-- | SimpleSize in vmin's (the smaller of vw or vh).
+vmin i = SimpleSize (cssDoubleText i <> "vmin")
 
--- | Size in vmax's (the larger of vw or vh).
-vmax i = Size (value i <> "vmax")
+-- | SimpleSize in vmax's (the larger of vw or vh).
+vmax i = SimpleSize (cssDoubleText i <> "vmax")
 
--- | Size in percents.
+-- | SimpleSize in percents.
 pct :: Double -> Size Percentage
-pct i = Size (value i <> "%")
+pct i = SimpleSize (cssDoubleText i <> "%")
 
 instance Num (Size LengthUnit) where
   fromInteger = px . fromInteger
@@ -151,6 +172,11 @@ instance Num (Size Percentage) where
 instance Fractional (Size Percentage) where
   fromRational = pct . fromRational
   recip  = error  "recip not implemented for Size"
+
+-- | Plus operator to combine sizes into calc function
+infixl 6 @+@
+(@+@) :: Size a -> Size a -> Size a
+a @+@ b = SumSize a b
 
 -------------------------------------------------------------------------------
 
