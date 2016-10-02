ad585a9eb5ee6cd3963fc3cf315f8ceea6480b36
Bob Long <robertjflong@gmail.com>
2016-10-02 15:57:30 +0100

    add htmlInline config

--- spec/Clay/RenderSpec.hs
+++ spec/Clay/RenderSpec.hs
@@ -1,12 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Clay.RenderSpec where
 
-import Clay.Render (renderWith, compact)
+import Clay.Render (renderWith, compact, htmlInline)
 import Data.Monoid
 import Test.Hspec
+import Clay
 
 spec :: Spec
 spec = do
     describe "compact" $ do
         it "with mempty produces empty compact CSS" $
             renderWith compact [] mempty `shouldBe` ""
+    describe "htmlInline" $ do
+        it "with rules produces compact inline css" $ do
+            let css = do background red
+                         color white
+            renderWith htmlInline [] css `shouldBe` "background:#ff0000;color:#ffffff"
