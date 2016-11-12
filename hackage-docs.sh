05e9e6049dbdf64d870408ba90ac29923716a69f
Sean Parsons <git@futurenotfound.com>
2016-11-12 14:34:58 +0000

    Added hackage-docs.sh script.

---
+++ hackage-docs.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Run this script in the top-level of your package directory
+# (where the .cabal file is) to compile documentation and
+# upload it to hackage.
+
+# Requirements:
+# cabal-install-1.24 (for --for-hackage)
+# haddock 2.17 (for the hyperlinked source)
+
+set -e
+
+dir=$(mktemp -d dist-docs.XXXXXX)
+trap 'rm -r "$dir"' EXIT
+
+cabal haddock --builddir="$dir" --for-hackage --haddock-option=--hyperlinked-source
+cabal upload -d $dir/*-docs.tar.gz
