3edf559308f42b67c5ebe9ef0a3e545f4f8c9bc3
NCrashed <ncrashed@gmail.com>
2019-12-20 16:38:12 +0300

    Add use case in module header

--- src/Clay/Grid.hs
+++ src/Clay/Grid.hs
@@ -1,4 +1,44 @@
 {-# LANGUAGE OverloadedStrings #-}
+-- | Partial implementation of <https://alligator.io/css/css-grid-layout-grid-areas grid area CSS API>.
+--
+-- For instance, you want to generate the following CSS:
+--
+-- @
+-- .grid1 {
+--   display: grid;
+--   width: max-content;
+-- }
+--
+-- .grid3 {
+--   display: grid;
+--   width: max-content;
+-- }
+--
+-- \@media (min-width: 40.0rem) {
+--   .grid3 {
+--     display: grid;
+--     grid-template-columns: 1fr 1fr 1fr;
+--     grid-gap: 1rem;
+--     width: max-content;
+--   }
+-- }
+-- @
+--
+-- The corresponding clay code:
+--
+-- @
+--  ".grid1" ? do
+--    display grid
+--    width maxContent
+--  ".grid3" ? do
+--    display grid
+--    width maxContent
+--  query M.screen [M.minWidth (rem 40)] $ ".grid3" ? do
+--    display grid
+--    gridTemplateColumns [fr 1, fr 1, fr 1]
+--    gridGap $ rem 1
+--    width maxContent
+-- @
 module Clay.Grid
 ( gridGap
 , gridTemplateColumns
