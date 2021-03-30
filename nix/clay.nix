51a06590b86c97c9c0b5a5bfe2c6b573502d53f6
Ben Sima <ben@bsima.me>
2021-03-30 09:27:49 -0400

    Don't use stdenv.lib
    This is a deprecated alias that should only be used for development. See the
    last bullet point of this manual section:
    
    https://nixos.org/manual/nixpkgs/unstable/#sec-syntax

--- nix/clay.nix
+++ nix/clay.nix
@@ -1,4 +1,4 @@
-{ mkDerivation, base, hspec, hspec-discover, mtl, stdenv, text }:
+{ mkDerivation, base, hspec, hspec-discover, mtl, stdenv, lib, text }:
 mkDerivation {
   pname = "clay";
   version = "0.14.0";
@@ -7,5 +7,5 @@ mkDerivation {
   testHaskellDepends = [ base hspec hspec-discover mtl text ];
   homepage = "http://fvisser.nl/clay";
   description = "CSS preprocessor as embedded Haskell";
-  license = stdenv.lib.licenses.bsd3;
+  license = lib.licenses.bsd3;
 }
