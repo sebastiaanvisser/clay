b316f8cfd393c5419681d3619562c0ff1bc846b8
Manuel Bärenz <programming@manuelbaerenz.de>
2022-03-16 15:00:30 +0100

    Fix whitespace & two warnings

--- spec/Clay/TransitionSpec.hs
+++ spec/Clay/TransitionSpec.hs
@@ -3,13 +3,12 @@ module Clay.TransitionSpec where
 
 import Test.Hspec
 import Clay
-import Common
 
 spec :: Spec
 spec = do
   describe "transition" $ do
     describe "generates proper code" $ do
-      it "renders cubic-bezier" $ 
-        (plain (unValue (value (cubicBezier 1.0 2.0 3.0 4.0))))
+      it "renders cubic-bezier" $
+        plain (unValue (value (cubicBezier 1.0 2.0 3.0 4.0)))
         `shouldBe`
         "cubic-bezier(1,2,3,4)"
