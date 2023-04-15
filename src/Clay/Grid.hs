ed216b5704213018a6c29e3e867a98e8bc2c4232
leo <leoanrd.monnier@gmail.com>
2023-04-15 21:29:02 +0200

    Clean code and improve documentation

--- src/Clay/Grid.hs
+++ src/Clay/Grid.hs
@@ -2,7 +2,6 @@
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ExtendedDefaultRules #-}
-{-# LANGUAGE TypeSynonymInstances #-}
 {-# OPTIONS_GHC -Wno-type-defaults #-}
 
 -- | Partial implementation of <https://alligator.io/css/css-grid-layout-grid-areas grid area CSS API>.
@@ -46,9 +45,18 @@
 --    width maxContent
 -- @
 module Clay.Grid
-(   -- * Style properties.
+(
+    -- * Grid
     gridGap
   , gridTemplateColumns
+
+    -- * Size and location
+
+    -- ** Data types and type classes
+  , GridLine (..)
+  , ToGridLine
+
+    -- ** Style properties
   , gridArea
   , gridColumn
   , gridColumnStart
@@ -57,7 +65,7 @@ module Clay.Grid
   , gridRowStart
   , gridRowEnd
 
-    -- * Keywords
+    -- ** Keywords
   , (//)
   , span_
 )
@@ -79,40 +87,259 @@ gridGap = key "grid-gap"
 gridTemplateColumns :: [Size a] -> Css
 gridTemplateColumns = key "grid-template-columns" . noCommas
 
+-- | A `grid-line` value.
+--
+-- A grid-line value specifies a size and location in a grid.
+-- the following are invalid:
+--
+-- NOTE: although you can use the below constructors, it's also possible
+-- to use a closer CSS syntax taking advantage of the 'ToGridLine' instances.
+data GridLine
+
+  -- | 'Integer' value.
+  --
+  -- NOTE: 'Integer' value of 0 is invalid.
+  = Coordinate Integer
+
+  -- | `custom-ident` with an optional 'Integer' value.
+  --
+  -- NOTE: 'Integer' value of 0 is invalid.
+  | CustomIndent String (Maybe Integer)
+
+  -- | `span` CSS keyword with an optional `custom-ident` and/or 'Integer' value.
+  --
+  -- NOTE: negative 'Integer' or 0 are invalid.
+  | Span (Maybe String) (Maybe Integer)
+
+  -- | `auto` CSS keyword.
+  | Auto
+
+  -- | `inherit` CSS keyword.
+  | Inherit
+
+  -- | `initial` CSS keyword.
+  | Initial
+
+  -- | `unset` CSS keyword.
+  | Unset
+  deriving (Eq, Show)
+
+class ToGridLine a where
+  -- | Convert the provided type to a 'GridLine'.
+  toGridLine :: a -> GridLine
+
+instance ToGridLine GridLine where
+  toGridLine = id
+
+instance ToGridLine Integer where
+  -- | NOTE: 'Integer' value of 0 is invalid.
+  toGridLine = Coordinate
+
+-- | `custom-ident` value.
+instance ToGridLine String where
+  toGridLine x = CustomIndent x Nothing
+
+-- | Both `custom-ident` and `Integer` values, provided as a pair.
+--
+-- NOTE: 'Integer' value of 0 is invalid.
+instance ToGridLine (String, Integer) where
+  toGridLine (x, y) = CustomIndent x (Just y)
+
+-- | One or two `grid-line` values.
+--
+-- NOTE: although you can use the below constructors, it's also possible
+-- to use a closer CSS syntax using the 'ToGridLines2' instances.
+data GridLines2
+    -- | One `grid-line` value.
+  = One2 OneGridLine
+    -- | Two `grid-line` values.
+  | Two2 TwoGridLines
+
+class ToGridLines2 a where
+
+  -- | Convert the provided type to 'GridLines2' (one or two `grid-line` values).
+  toGridLines2 :: a -> GridLines2
+
+instance ToGridLines2 GridLine where
+  -- | One `grid-line` value.
+  toGridLines2 = One2 . OneGridLine
+
+instance ToGridLines2 OneGridLine where
+  -- | One `grid-line` value.
+  toGridLines2 = One2
+
+instance ToGridLines2 TwoGridLines where
+  -- | Two `grid-line` values.
+  toGridLines2 = Two2
+
+instance ToGridLines2 GridLines2 where
+  -- | One or two `grid-line` values.
+  toGridLines2 = id
+
+instance ToGridLines2 Integer where
+  -- | One 'Integer' value.
+  --
+  -- NOTE: 'Integer' value of 0 is invalid.
+  toGridLines2 = toGridLines2 . toGridLine
+
+instance ToGridLines2 String where
+  -- | One `custom-ident` value.
+  toGridLines2 = toGridLines2 . toGridLine
+
+instance ToGridLines2 (String, Integer) where
+  -- | One time both a `custom-ident` and 'Integer' values, provided as a pair.
+  --
+  -- NOTE: 'Integer' value of 0 is invalid.
+  toGridLines2 = toGridLines2 . toGridLine
+
+-- | One, two, three or four `grid-line` values.
+--
+-- NOTE: although you can use the below constructors, it's also possible
+-- to use a closer CSS syntax using the 'ToGridLines4' instances.
+data GridLines4
+    -- | One `grid-line` value.
+  = One OneGridLine
+    -- | Two `grid-line` values.
+  | Two TwoGridLines
+    -- | Three `grid-line` values.
+  | Three ThreeGridLines
+    -- | Four `grid-line` values.
+  | Four FourGridLines
+
+class ToGridLines4 a where
+  -- | Convert the provided type to 'GridLines4'
+  -- (one, two, three or four `grid-line` values).
+  toGridLines :: a -> GridLines4
+
+instance ToGridLines4 GridLine where
+  -- | One `grid-line` value.
+  toGridLines = One . OneGridLine
+
+instance ToGridLines4 OneGridLine where
+  -- | One `grid-line` value.
+  toGridLines = One
+
+instance ToGridLines4 TwoGridLines where
+  -- | Two `grid-line` values.
+  toGridLines = Two
+
+instance ToGridLines4 ThreeGridLines where
+  -- | Three `grid-line` values.
+  toGridLines = Three
+
+instance ToGridLines4 FourGridLines where
+  -- | Four `grid-line` values.
+  toGridLines = Four
+
+instance ToGridLines4 GridLines4 where
+  -- | One, two, three or four `grid-line` values.
+  toGridLines = id
+
+instance ToGridLines4 Integer where
+  -- | One 'Integer' value.
+  toGridLines = toGridLines . toGridLine
+
+instance ToGridLines4 String where
+  -- | One `custom-ident` value.
+  toGridLines = toGridLines . toGridLine
+
+instance ToGridLines4 (String, Integer) where
+  -- | One time both a `custom-ident` and 'Integer' values, provided as a pair.
+  toGridLines = toGridLines . toGridLine
+
+-- | One 'GridLine' value.
+newtype OneGridLine = OneGridLine GridLine
+
+-- | Two 'GridLine' values.
+data TwoGridLines = TwoGridLines GridLine GridLine
+
+-- | Three 'GridLine' values.
+data ThreeGridLines = ThreeGridLines GridLine GridLine GridLine
+
+-- | Four 'GridLine' values.
+data FourGridLines = FourGridLines GridLine GridLine GridLine GridLine
+
 -- | Property shorthand specifies a grid item's size and location
 -- within a grid.
+--
+-- One to four `grid-line` values can be specified.
+-- Grid-line values must be separated by a '(//)' operator.
+--
+-- WARNING: this function is partial. TODO report cases.
+--
+-- ==== __Examples__
+--
+-- >>> gridArea 3
+--
+-- >>> gridArea2 $ 2 // "nav"
+--
+-- >>> gridArea $ ("nav", 2) // span_ 3 // 4
 gridArea :: ToGridLines4 a => a -> Css
 gridArea x = key "grid-area" (toGridLines x)
 
 -- | Property shorthand specifies a grid item's size and location
 -- within a grid column.
+--
+-- TODO write doc.
 gridColumn :: ToGridLines2 a => a -> Css
 gridColumn x = key "grid-column" (toGridLines2 x)
 
 -- | Property specifies a grid item's start position within the grid column.
+--
+-- TODO: write doc.
 gridColumnStart :: ToGridLine a => a -> Css
 gridColumnStart x = key "grid-column-start" (toGridLine x)
 
 -- | Property specifies a grid item's end position within the grid column.
+--
+-- TODO: write doc.
 gridColumnEnd :: ToGridLine a => a -> Css
 gridColumnEnd x = key "grid-column-end" (toGridLine x)
 
 -- | Property shorthand specifies a grid item's size and location
 -- within a grid row.
+--
+-- One or two `grid-line` values can be specified.
+-- `grid-line` values must be separated by a '(//)' operator.
+--
+-- WARNING: this function is partial. TODO provide cases.
+--
+-- ==== __Examples__
+--
+-- >>> gridRow "nav"
+--
+-- >>> gridRow $ 3 // span_ 2
 gridRow :: ToGridLines2 a => a -> Css
 gridRow x = key "grid-row" (toGridLines2 x)
 
 -- | Property specifies a grid item's start position within the grid row.
+--
+-- WARNING: this function is partial, the following are invalid:
+--
+-- - an 'Integer' value of 0
+-- - a pair with an 'Integer' component of value 0
+-- - a 'span_' function provided with an 'Integer' value of 0 or negative
+-- - a 'span_' function provided with a pair value with
+-- an 'Integer' component of 0 or negative.
+-- - a 'GridLine' value representing one of the above.
 gridRowStart :: ToGridLine a => a -> Css
-gridRowStart x = key "grid-row-start" (toGridLine x)
+gridRowStart x = key "grid-row-start" (partialToGridLine x)
 
 -- | Property specifies a grid item's end position within the grid row.
+--
+-- WARNING: this function is partial, the following arguments are invalid:
+-- - an 'Integer' value of 0
+-- - a pair with an 'Integer' component of value 0
+-- - a 'span_' function provided with an 'Integer' value of 0 or negative
+-- - a 'span_' function provided with a pair value with
+-- an 'Integer' component of 0 or negative
+-- - a 'GridLine' value representing one of the above.
 gridRowEnd :: ToGridLine a => a -> Css
-gridRowEnd x = key "grid-row-end" (toGridLine x)
+gridRowEnd x = key "grid-row-end" (partialToGridLine x)
 
 class Slash a r | a -> r where
-  -- | CSS equivalent /.
-  -- Separates `GridLine` values.
+  -- | `/` CSS operator.
+  -- Separates `grid-line` values.
   (//) :: ToGridLine b => a -> b -> r
 
 instance Slash GridLine TwoGridLines where
@@ -133,11 +360,6 @@ instance Slash TwoGridLines ThreeGridLines where
 instance Slash ThreeGridLines FourGridLines where
   (ThreeGridLines xx xy xz) // y = FourGridLines xx xy xz (toGridLine y)
 
--- | Keyword indicating that the property contributes nothing to the grid item's
--- placement.
-instance Com.Auto GridLine where
-  auto = Auto
-
 class ToSpan a where
 
   -- | Contributes to the grid item's placement.
@@ -146,6 +368,8 @@ class ToSpan a where
 instance ToSpan Integer where
 
   -- | Contributes the nth grid line to the grid item's placement.
+  --
+  -- NOTE: negative 'Integer' or 0 values are invalid.
   span_ x = Span Nothing (Just x)
 
 instance ToSpan String where
@@ -156,70 +380,28 @@ instance ToSpan String where
 instance ToSpan (String, Integer) where
 
   -- | Nth lines from the provided name are counted.
+  --
+  -- NOTE: negative 'Integer' or 0 values are invalid.
   span_ (x, y) = Span (Just x) (Just y)
 
--- | Keyword inherit applied to a `GridLine`.
+-- | Keyword indicating that the property contributes nothing to the grid item's
+-- placement.
+instance Com.Auto GridLine where
+  auto = Auto
+
+-- | Keyword `inherit` applied to a 'GridLine'.
 instance Com.Inherit GridLine where
   inherit = Inherit
 
--- | Keyword initial applied to a `GridLine`.
+-- | Keyword `initial` applied to a 'GridLine'.
 instance Com.Initial GridLine where
   initial = Initial
 
--- | Keyword unset applied to a `GridLine`.
+-- | Keyword `unset` applied to a 'GridLine'.
 instance Com.Unset GridLine where
   unset = Unset
 
--- | Grid line value.
-data GridLine =
-    Auto
-  | Inherit
-  | Initial
-  | Unset
-  | Coordinate Integer
-  | CustomIndent String (Maybe Integer)
-  | Span (Maybe String) (Maybe Integer)
-  deriving (Eq, Show)
-
-class ToGridLine a where
-  toGridLine :: a -> GridLine
-
-instance ToGridLine GridLine where
-  toGridLine = id
-
-instance ToGridLine Integer where
-  toGridLine = Coordinate
-
-instance ToGridLine String where
-  toGridLine x = CustomIndent x Nothing
-
-instance ToGridLine (String, Integer) where
-  toGridLine (x, y) = CustomIndent x (Just y)
-
--- | One or two `GridLine` values.
-data GridLines2 =
-    One2 OneGridLine
-  | Two2 TwoGridLines
-
--- | One, two, three or four `GridLine` values.
-data GridLines4 =
-    One OneGridLine
-  | Two TwoGridLines
-  | Three ThreeGridLines
-  | Four FourGridLines
-
--- | One `GridLine` value
-newtype OneGridLine = OneGridLine GridLine
-
--- | Two `GridLine` values.
-data TwoGridLines = TwoGridLines GridLine GridLine
-
--- | Three `GridLine` values.
-data ThreeGridLines = ThreeGridLines GridLine GridLine GridLine
-
--- | Four `GridLine` values.
-data FourGridLines = FourGridLines GridLine GridLine GridLine GridLine
-
+-- | Convertion of 'GridLine' to 'Clay.Property.Value'.
 instance Val GridLine where
   value Auto               = "auto"
   value Inherit            = "inherit"
@@ -229,62 +411,6 @@ instance Val GridLine where
   value (CustomIndent x y) = value $ T.pack x <> foldMap ((" " <>) . tshow) y
   value (Span x y) =
     value $ "span" <> foldMap ((" " <>) . T.pack) x <> foldMap ((" " <>) . tshow) y
-
--- | Convert a value to `GridLines2` (one or two `GridLine`).
-class ToGridLines2 a where
-  toGridLines2 :: a -> GridLines2
-
-instance ToGridLines2 GridLines2 where
-  toGridLines2 = id
-
-instance ToGridLines2 GridLine where
-  toGridLines2 = One2 . OneGridLine
-
-instance ToGridLines2 OneGridLine where
-  toGridLines2 = One2
-
-instance ToGridLines2 TwoGridLines where
-  toGridLines2 = Two2
-
-instance ToGridLines2 Integer where
-  toGridLines2 = toGridLines2 . toGridLine
-
-instance ToGridLines2 String where
-  toGridLines2 = toGridLines2 . toGridLine
-
-instance ToGridLines2 (String, Integer) where
-  toGridLines2 = toGridLines2 . toGridLine
-
--- | Convert a value to `GridLines4` (one, two, three or four `GridLine`).
-class ToGridLines4 a where
-  toGridLines :: a -> GridLines4
-
-instance ToGridLines4 GridLines4 where
-  toGridLines = id
-
-instance ToGridLines4 GridLine where
-  toGridLines = One . OneGridLine
-
-instance ToGridLines4 OneGridLine where
-  toGridLines = One
-
-instance ToGridLines4 TwoGridLines where
-  toGridLines = Two
-
-instance ToGridLines4 ThreeGridLines where
-  toGridLines = Three
-
-instance ToGridLines4 FourGridLines where
-  toGridLines = Four
-
-instance ToGridLines4 Integer where
-  toGridLines = toGridLines . toGridLine
-
-instance ToGridLines4 String where
-  toGridLines = toGridLines . toGridLine
-
-instance ToGridLines4 (String, Integer) where
-  toGridLines = toGridLines . toGridLine
 
 instance Val OneGridLine where
   value (OneGridLine x) = value x
@@ -316,6 +442,22 @@ instance Val GridLines4 where
   value (Three x) = value x
   value (Four x)  = value x
 
--- | Utility function to show Text instead of String.
+-- | Private partial function converting its argument to 'GridLine'.
+--
+-- An error is raised when:
+-- - An 'Integer' value of 0 is provided.
+-- - A negative 'Integer' value is provided for a 'Span' constructor.
+partialToGridLine :: ToGridLine a => a -> GridLine
+partialToGridLine x = case toGridLine x of
+  Coordinate 0            -> errorValue 0
+  CustomIndent _ (Just 0) -> errorValue 0
+  s@(Span _ (Just n))     -> if n < 1
+                             then errorValue n
+                             else s
+  gridLine                -> gridLine
+  where
+    errorValue n = error ("Value " ++ show n ++ " is invalid")
+
+-- | Private utility function to show 'Text' instead of 'String'.
 tshow :: Show a => a -> Text
 tshow = T.pack . show
