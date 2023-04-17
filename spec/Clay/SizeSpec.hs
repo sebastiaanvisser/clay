d5833587466bb01bdce1e2a58bc7288c56dc18d4
leo <leoanrd.monnier@gmail.com>
2023-04-17 21:40:07 +0200

    Explicit import of the sort function
    This avoids a warning from the compiler.

--- spec/Clay/SizeSpec.hs
+++ spec/Clay/SizeSpec.hs
@@ -13,7 +13,7 @@ import Clay.Stylesheet
 import Test.Hspec
 import Data.Text
 import Data.Text.Lazy (toStrict)
-import Data.List
+import Data.List (sort)
 
 sizeRepr :: Size a -> Text
 sizeRepr = plain . unValue . value
